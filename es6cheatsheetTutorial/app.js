f6902a52923945c7c744cdd31c9b98a24da9979b
Austin Beaufort <tinyhatcoding@gmail.com>
2019-02-02 22:20:19 -0500

    completed tutorial

---
+++ es6cheatsheetTutorial/app.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// DESTRUCTURING
+
+// let foo = {
+//     bar: 1,
+//     baz: 2
+// };
+
+// let {bar, baz} = foo;
+
+// console.log(bar);
+// console.log(baz);
+
+
+// let tenses = ['me', 'you', 'he'];
+// let [firstPerson, secondPerson, thirdPerson] = tenses;
+
+// console.log(firstPerson); // me
+// console.log(secondPerson);
+// console.log(thirdPerson);
+
+// let foo = 2;
+
+// let obj = {
+//     bar: 1,
+//     foo // automatically assigns 2 to foo
+// }
+
+// console.log(obj.foo);
+
+// let name = 'will';
+
+// let obj = {
+//     ["name" + name]: "some value",
+// }
+
+
+// function calcBmi(weight, height, callback) {
+//     let bmi = weight / Math.pow(height, 2);
+//     if(callback) {
+//         callback(bmi);
+//     }
+// }
+
+// calcBmi(weight, height);
+// calcBmi(weight, height, function() {});
+
+
+// class Parent {
+//     constructor() {
+
+//     }
+
+//     foo() {
+        
+//     }
+
+//     bar() {
+
+//     }
+// }
